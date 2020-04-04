6930cffa495b4941dc86dcd5f7c7cfe0943abb40
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2020-04-05 00:00:24 +0200

    Restructure test suite.

--- test/api_java.jl
+++ test/api_java.jl
@@ -1,61 +1,67 @@
 @testset "Java API" begin
-    ## Model initialisation
-    cpo_java_init()
-    model = cpo_java_model()
-    @test typeof(model) == JavaCPOModel
+    @testset "Unit tests" begin
+        @testset "Model initialisation" begin
+            cpo_java_init()
+            model = cpo_java_model()
+            @test typeof(model) == JavaCPOModel
 
-    # Types
-    @test model.intvar == JavaCall.JavaObject{Symbol("ilog.concert.IloIntVar")}
-    @test model.intervalvar == JavaCall.JavaObject{Symbol("ilog.concert.IloIntervalVar")}
-    @test model.numvar == JavaCall.JavaObject{Symbol("ilog.concert.IloNumVar")}
+            # Types
+            @test model.intvar == JavaCall.JavaObject{Symbol("ilog.concert.IloIntVar")}
+            @test model.intervalvar == JavaCall.JavaObject{Symbol("ilog.concert.IloIntervalVar")}
+            @test model.numvar == JavaCall.JavaObject{Symbol("ilog.concert.IloNumVar")}
 
-    @test model.intexpr == JavaCall.JavaObject{Symbol("ilog.concert.IloIntExpr")}
-    @test model.numexpr == JavaCall.JavaObject{Symbol("ilog.concert.IloNumExpr")}
-    @test model.inttupleset == JavaCall.JavaObject{Symbol("ilog.concert.IloIntTupleSet")}
+            @test model.intexpr == JavaCall.JavaObject{Symbol("ilog.concert.IloIntExpr")}
+            @test model.numexpr == JavaCall.JavaObject{Symbol("ilog.concert.IloNumExpr")}
+            @test model.inttupleset == JavaCall.JavaObject{Symbol("ilog.concert.IloIntTupleSet")}
 
-    @test model.constraint == JavaCall.JavaObject{Symbol("ilog.concert.IloConstraint")}
-    @test model.alternative == JavaCall.JavaObject{Symbol("ilog.concert.IloAlternative")}
+            @test model.constraint == JavaCall.JavaObject{Symbol("ilog.concert.IloConstraint")}
+            @test model.alternative == JavaCall.JavaObject{Symbol("ilog.concert.IloAlternative")}
 
-    @test model.intvararray == Vector{model.intvar}
-    @test model.numvararray == Vector{model.numvar}
-    @test model.intexprarray == Vector{model.intexpr}
-    @test model.numexprarray == Vector{model.numexpr}
+            @test model.intvararray == Vector{model.intvar}
+            @test model.numvararray == Vector{model.numvar}
+            @test model.intexprarray == Vector{model.intexpr}
+            @test model.numexprarray == Vector{model.numexpr}
+        end
 
-    ## Variable creation
-    # Integer variables
-    @test typeof(cpo_java_intvar(model, 5, 10)) == model.intvar
-    @test typeof(cpo_java_intvar(model, 10, 5)) == model.intvar # No check on CPLEXCP side
-    @test typeof(cpo_java_intvar(model, 5, 10, "var")) == model.intvar
+        @testset "Variable creation" begin
+            model = cpo_java_model()
 
-    @test typeof(cpo_java_intvar(model, [5, 10])) == model.intvar
-    @test typeof(cpo_java_intvar(model, [5, 10], "var")) == model.intvar
+            # Integer variables
+            @test typeof(cpo_java_intvar(model, 5, 10)) == model.intvar
+            @test typeof(cpo_java_intvar(model, 10, 5)) == model.intvar # No check on CPLEXCP side
+            @test typeof(cpo_java_intvar(model, 5, 10, "var")) == model.intvar
 
-    l = cpo_java_intvararray(model, 20, 5, 10)
-    @test typeof(l) == model.intvararray
-    @test eltype(l) == model.intvar
-    @test length(l) == 20
-    l = cpo_java_intvararray(model, 20, [5, 10])
-    @test typeof(l) == model.intvararray
-    @test eltype(l) == model.intvar
-    @test length(l) == 20
+            @test typeof(cpo_java_intvar(model, [5, 10])) == model.intvar
+            @test typeof(cpo_java_intvar(model, [5, 10], "var")) == model.intvar
 
-    # Numerical variables
-    @test typeof(cpo_java_numvar(model, 5.0, 10.0)) == model.numvar
+            l = cpo_java_intvararray(model, 20, 5, 10)
+            @test typeof(l) == model.intvararray
+            @test eltype(l) == model.intvar
+            @test length(l) == 20
+            l = cpo_java_intvararray(model, 20, [5, 10])
+            @test typeof(l) == model.intvararray
+            @test eltype(l) == model.intvar
+            @test length(l) == 20
 
-    l = cpo_java_numvararray(model, 20, 5.0, 10.0)
-    @test typeof(l) == model.numvararray
-    @test eltype(l) == model.numvar
-    @test length(l) == 20
+            # Numerical variables
+            @test typeof(cpo_java_numvar(model, 5.0, 10.0)) == model.numvar
 
-    # Interval variables
-    @test typeof(cpo_java_intervalvar(model)) == model.intervalvar
-    @test typeof(cpo_java_intervalvar(model, "var")) == model.intervalvar
-    @test typeof(cpo_java_intervalvar(model, 5)) == model.intervalvar
-    @test typeof(cpo_java_intervalvar(model, 5, "var")) == model.intervalvar
-    @test typeof(cpo_java_intervalvar(model, 5, 10)) == model.intervalvar
+            l = cpo_java_numvararray(model, 20, 5.0, 10.0)
+            @test typeof(l) == model.numvararray
+            @test eltype(l) == model.numvar
+            @test length(l) == 20
 
-    # Sequence-of-intervals variables
-    i1 = cpo_java_intervalvar(model)
-    i2 = cpo_java_intervalvar(model)
-    @test typeof(cpo_java_intervalsequencevar(model, [i1, i2])) == model.intervalsequencevar
+            # Interval variables
+            @test typeof(cpo_java_intervalvar(model)) == model.intervalvar
+            @test typeof(cpo_java_intervalvar(model, "var")) == model.intervalvar
+            @test typeof(cpo_java_intervalvar(model, 5)) == model.intervalvar
+            @test typeof(cpo_java_intervalvar(model, 5, "var")) == model.intervalvar
+            @test typeof(cpo_java_intervalvar(model, 5, 10)) == model.intervalvar
+
+            # Sequence-of-intervals variables
+            i1 = cpo_java_intervalvar(model)
+            i2 = cpo_java_intervalvar(model)
+            @test typeof(cpo_java_intervalsequencevar(model, [i1, i2])) == model.intervalsequencevar
+        end
+    end
 end
