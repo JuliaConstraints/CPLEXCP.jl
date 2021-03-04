31655485ade9c3a3c403d08402a9c6cecd28b60d
Thibaut Cuvelier <cuvelier.thibaut@gmail.com>
2021-03-04 01:00:36 +0100

    Fix from JuliaFormatter.

--- src/api_java.jl
+++ src/api_java.jl
@@ -111,7 +111,7 @@ function cpo_java_model()
 
     # Getting access to the values is not possible before the JVM is started, i.e. before cpo_java_init() is called.
     # It is thus not possible to use const for these values, unfortunately. TODO: Or just hard-code the values?
-    conflictstatus = @jimport ilog.cp.IloCP $ConflictStatus
+    conflictstatus = @jimport ilog.cp.IloCP$ConflictStatus
     conflictstatus_possible =
         jfield(conflictstatus, "ConflictPossibleMember", conflictstatus)
     conflictstatus_member =
